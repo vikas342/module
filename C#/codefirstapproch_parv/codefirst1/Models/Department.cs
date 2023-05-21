97ee9976448076bf82ebb2b2a7f57c25ea9ea730
vikas342 <vsonwane3660@gmail.com>
2023-05-21 19:43:24 +0530

    21-05

---
+++ C#/codefirstapproch_parv/codefirst1/Models/Department.cs
@@ -0,0 +1,17 @@
+﻿
+
+using System.ComponentModel.DataAnnotations;
+
+namespace codefirst1.Models
+{
+    public class Department
+    {
+        
+        public int Id { get; set; }
+
+        [Required]
+        public string? DeptName { get; set; }
+
+        public ICollection<Employee>? Employee { get; set; }
+    }
+}
---
+++ C#/codefirstapproch_parv/codefirst1/Models/Employee.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace codefirst1.Models
+{
+    public class Employee
+    {
+        public int Id { get; set; }
+        [Required]
+        public string? Name { get; set; }
+        public int DepartmentId { get; set; }
+        [Required]
+        public string? Password { get; set; }
+
+        public Department? Department { get; set; }
+
+    }
+}
